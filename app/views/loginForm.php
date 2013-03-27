6ce14ff9e34277591ef36f32d63861578b8267db
Joshua Ellis <jzellis@gmail.com>
2013-03-27 19:10:42 -0400

    Changing herpy derpy wording for complete and utter useless moron assholes

--- app/views/loginForm.php
+++ app/views/loginForm.php
@@ -1,12 +1,12 @@
-      <H1>LOGIN OR SIGN UP WITH FACEBOOK OR TWITTER TO FUND THIS GOAL</H1>
+      <H1>LOGIN OR SIGN UP TO FUND THIS GOAL</H1>
   		<a class='btn btn-info btn-large' href='/auth/facebook' style='background: #596F90; margin-top: 0.5em; margin-bottom: 0.5em'><i class='icon-facebook'></i> Log In With Facebook</a> <a class='btn btn-info btn-large' href='/auth/twitter'><i class='icon-twitter'></i> Log In With Twitter</a>
   		<hr>
 
-<h2>Or Login Or Signup To Fund This Goal</h2>
+<h2>Or Use Email And Password</h2>
 	<form action='/login' method='post'>
   		<input type='text' class='input-xlarge' name='login' placeholder='Login'><br>
   		<input type='password' class='input-xlarge' name='password' placeholder='Password'><br>
-  		<button class='btn btn-small' type='submit'>Login</button> or <a href='/signup' class='btn btn-info'>Signup</a><br>
+  		<button class='btn' type='submit'>Login</button> or <a href='/signup' class='btn btn-info'>Signup</a><br>
 
 
 <hr class='clearfix'>
